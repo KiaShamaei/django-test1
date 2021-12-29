23a8d3db066d08f2816b9b4fc7bc071129b60e37
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-12-29 18:45:19 +0330

    add post method form request

--- calc/urls.py
+++ calc/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home , name='home'),
     path('test/' , views.test , name ='test'),
-    path('add' , views.add , name ='add')
+    path('add' , views.add , name ='add'),
+    path('formpost' , views.formPost , name='formPost') ,
+    path('addpost' , views.addPost , name='formPost') 
 ]
--- calc/views.py
+++ calc/views.py
@@ -6,8 +6,15 @@ def home(request):
     return render(request, 'home.html' , {'name' : "hassan"})
 def test(request):
     return HttpResponse('<h1>this is a test page </h2>')
+def formPost(request):
+    return render(request, 'formPost.html' )
 def add(request):
     val1 = int(request.GET['num1'])
     val2 = int(request.GET['num2'])
+    result = val1 + val2
+    return render(request , 'result.html'  , {'result' : result})
+def addPost(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     result = val1 + val2
     return render(request , 'result.html'  , {'result' : result})
