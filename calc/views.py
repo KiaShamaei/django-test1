463847cd62217eaa0c67431c171b1e893e6f446a
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-12-29 18:28:03 +0330

    add get request by form

--- calc/views.py
+++ calc/views.py
@@ -5,4 +5,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html' , {'name' : "hassan"})
 def test(request):
-    return HttpResponse("<h2>this is test ----</h2>")
+    return HttpResponse('<h1>this is a test page </h2>')
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    result = val1 + val2
+    return render(request , 'result.html'  , {'result' : result})
