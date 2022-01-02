e0cf1b51819818df6efc9a806efc8e767c92e661
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-02 19:47:09 +0330

    add static and pass dynamic data

--- travelo/views.py
+++ travelo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 def travelo(request):
-    return render(request, 'index.html' )
+    dest1 = Destination()
+    dest1.name= "kia"
+    dest1.desc = "is very smart person"
+    dest1.price = 7000
+    return render(request, 'index.html' , {'dest1' : dest1} )
