8cf510322b4c41e17fc32420d8cd8919d1981cb8
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-03 19:42:03 +0330

    add if else django

--- travelo/views.py
+++ travelo/views.py
@@ -8,15 +8,18 @@ def travelo(request):
     dest1.desc = "is very smart person"
     dest1.img = "customer1.jpeg"
     dest1.price = 7000
+    dest1.offer = True
     dest2 = Destination()
     dest2.name= "ahmad"
     dest2.desc = "is very smart young"
     dest2.img = "customer2.jpeg"
     dest2.price = 6500
+    dest2.offer = False
     dest3 = Destination()
     dest3.name= "kia"
     dest3.desc = "is very smart big"
     dest3.img = "customer3.jpeg"
     dest3.price = 5000
+    dest3.offer = False
     dests = [dest1,dest2,dest3]
     return render(request, 'index.html' , {'dests' : dests} )
