a37ad140beb6987d008ecf8e0ac464c3ed0a2809
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-03 19:31:07 +0330

    add list and it with loop

--- travelo/views.py
+++ travelo/views.py
@@ -6,5 +6,17 @@ def travelo(request):
     dest1 = Destination()
     dest1.name= "kia"
     dest1.desc = "is very smart person"
+    dest1.img = "customer1.jpeg"
     dest1.price = 7000
-    return render(request, 'index.html' , {'dest1' : dest1} )
+    dest2 = Destination()
+    dest2.name= "ahmad"
+    dest2.desc = "is very smart young"
+    dest2.img = "customer2.jpeg"
+    dest2.price = 6500
+    dest3 = Destination()
+    dest3.name= "kia"
+    dest3.desc = "is very smart big"
+    dest3.img = "customer3.jpeg"
+    dest3.price = 5000
+    dests = [dest1,dest2,dest3]
+    return render(request, 'index.html' , {'dests' : dests} )
