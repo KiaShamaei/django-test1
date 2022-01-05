e8a0e832ffe20738ab8c6039f23a5bbb200ba444
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-05 19:21:32 +0330

    connect to postgres

--- travelo/views.py
+++ travelo/views.py
@@ -8,13 +8,13 @@ def travelo(request):
     dest1.desc = "is very smart person"
     dest1.img = "customer1.jpeg"
     dest1.price = 7000
-    dest1.offer = True
+    dest1.offer = False
     dest2 = Destination()
     dest2.name= "ahmad"
     dest2.desc = "is very smart young"
     dest2.img = "customer2.jpeg"
     dest2.price = 6500
-    dest2.offer = False
+    dest2.offer = True
     dest3 = Destination()
     dest3.name= "kia"
     dest3.desc = "is very smart big"
