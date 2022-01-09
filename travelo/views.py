46ebfec40d214ae03821416134a5c8c96bbc4d10
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-09 19:38:28 +0330

    add admin part get data and show it

--- travelo/views.py
+++ travelo/views.py
@@ -3,23 +3,5 @@ from django.http import HttpResponse
 from .models import Destination
 
 def travelo(request):
-    dest1 = Destination()
-    dest1.name= "kia"
-    dest1.desc = "is very smart person"
-    dest1.img = "customer1.jpeg"
-    dest1.price = 7000
-    dest1.offer = False
-    dest2 = Destination()
-    dest2.name= "ahmad"
-    dest2.desc = "is very smart young"
-    dest2.img = "customer2.jpeg"
-    dest2.price = 6500
-    dest2.offer = True
-    dest3 = Destination()
-    dest3.name= "kia"
-    dest3.desc = "is very smart big"
-    dest3.img = "customer3.jpeg"
-    dest3.price = 5000
-    dest3.offer = False
-    dests = [dest1,dest2,dest3]
+    dests = Destination.objects.all()
     return render(request, 'index.html' , {'dests' : dests} )
